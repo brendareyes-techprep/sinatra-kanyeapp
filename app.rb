dd2d01dc2de97e3695740ecc950a4d87d7293b30
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-09-25 17:22:38 +0000

    render

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/quote_form") do
+  erb(:quote_form)
+end
+
+get("/quote_result") do
+  kanye_url = "https://api.kanye.rest"
+  @raw_response = HTTP.get(kanye_url).to_s
+  @parsed_response = JSON.parse(@raw_response)
+  @quote = @parsed_response.fetch("quote")
+  erb(:quote_result)
 end
